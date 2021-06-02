64cb630c5b98a7bcf004509281169b25aaec7e6e
nolastemgarden <nolastemgarden@gmail.com>
2021-06-02 10:05:05 -0500

    Remove unused imports

--- src/theme.js
+++ src/theme.js
@@ -1,17 +1,10 @@
-import React from 'react';
-
-
 import {
     createMuiTheme,
-    makeStyles,
     responsiveFontSizes,
 } from '@material-ui/core/styles';
 
-
-
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
-// import { Button, createStyles } from '@material-ui/core';
+// import purple from '@material-ui/core/colors/purple';
+// import green from '@material-ui/core/colors/green';
 
 const theme = createMuiTheme({
     palette: {
