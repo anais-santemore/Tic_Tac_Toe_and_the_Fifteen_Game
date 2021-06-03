f9a20351ff6373456ca2f8be265a61958f1c141b
nolastemgarden <nolastemgarden@gmail.com>
2021-06-03 17:35:12 -0500

    Import shared buttons
    Extracted NewGameButton and UndoButton from HumanPanel into their own files and imported them here for reuse.

--- src/components/Panels/CoachPanel.js
+++ src/components/Panels/CoachPanel.js
@@ -2,12 +2,18 @@
 
 import React, { useState } from 'react';
 
-// Custom Components
+// My Components
 import HelpModal from "../HelpModal";
 import TicTacToeSettingsModal from "../TicTacToeSettingsModal";
 
+import NewGameButton from "../Buttons/NewGameButton";
+import UndoButton from "../Buttons/UndoButton";
+import ShowHintsButton from "../Buttons/ShowHintsButton";
+
+
 // MUI Components
 import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
@@ -32,26 +38,24 @@ const useStyles = makeStyles((theme) => ({
     },
     infoArea: {
         // border: 'solid red 1px',
-        flex: '1 0 65%',
+        flex: '1 0 55%',
         display: 'flex',
         flexDirection: 'column',
-        padding: '1.0rem 2.0rem 0.0rem ',
+        // padding: '1.0rem 2.0rem 0.0rem ',
 
     },
-    controls: {
-        // border: 'solid green 1px',
-        flex: '1 0 35%',
+    buttonArea: {
+        // border: 'solid red 1px',
+        flex: '1 0 45%',
         display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+        // padding: '1.0rem 2.0rem 0.0rem ',
 
+    },
     button: {
         color: theme.palette.common.white,
         backgroundColor: theme.palette.primary.main,
-        margin: '0.5rem 1.0rem',
-        width: '50%',
+        margin: '0.5rem 5%',
+        width: '90%',
         height: '3.0rem',
         fontSize: '1.2rem',
     },
@@ -61,22 +65,24 @@ const useStyles = makeStyles((theme) => ({
         // fontSize: 'min(max(0.7rem, 3vmin), 22px)',
     },
 
+
 }));
 
-export default function Panel(props) {
+export default function CoachPanel(props) {
     const classes = useStyles();
 
-    const mode = props.mode
-    const gameType = props.gameType;
+    const gameNumber = props.gameNumber;
+    const record = props.record;
+
 
     const gameOver = props.gameOver;
     const moveNumber = props.moveNumber;
     const gameStatus = props.gameStatus;
-    const gameNumber = props.gameNumber;
     const commentary = props.commentary;
 
     const handleNewGameClick = props.handleNewGameClick
     const handleUndoClick = props.handleUndoClick
+
 
     const showHints = props.showHints
     const toggleShowHints = props.toggleShowHints
@@ -85,16 +91,53 @@ export default function Panel(props) {
     const showCommentary = props.showCommentary
     const toggleShowCommentarySwitch = props.toggleShowCommentarySwitch
 
+
     const scoreBoard = (
         <React.Fragment>
-            <Typography align='center' component='h1' variant='h3' noWrap gutterBottom>
-                Game {gameNumber}:&nbsp;&nbsp;{gameStatus}
+            <Typography align='center' component='h1' variant='h4' noWrap gutterBottom>
+                Game Number:&nbsp;{gameNumber}
             </Typography>
-            <Typography align='center' component='h3' variant='h4' noWrap >
-                Human: {props.record[0]} &emsp;  Bot: {props.record[1]} &emsp;  Draw: {props.record[2]}
+            <Typography align='center' component='h1' variant='h4' noWrap gutterBottom>
+                {gameStatus}
+            </Typography>
+            <Typography align='center' component='h3' variant='h5' noWrap >
+                Player X: &ensp;{props.record[0]}
+            </Typography>
+            <Typography align='center' component='h3' variant='h5' noWrap >
+                Player O: &ensp;{props.record[1]}
+            </Typography>
+            <Typography align='center' component='h3' variant='h5' noWrap gutterBottom>
+                Draw: &emsp; &ensp;  {props.record[2]}
             </Typography>
         </React.Fragment>
     )
+
+
+    return (
+        <Container maxWidth='sm' className={classes.panel} >
+            <Box className={classes.infoArea} >
+                {scoreBoard}
+            </Box>
+            <Grid container className={classes.buttonArea} >
+                <Grid item xs={12} sm={6}  >
+                    <NewGameButton gameOver={gameOver}
+                        handleNewGameClick={handleNewGameClick}
+                    />
+
+                </Grid>
+                <Grid item xs={12} sm={6}   >
+                    <UndoButton gameOver={gameOver}
+                        moveNumber={moveNumber}
+                        handleUndoClick={handleUndoClick}
+                    />
+                </Grid>
+
+
+            </Grid>
+        </Container>
+    )
+
+    
 
     const commentaryBoard = (
         <React.Fragment>
@@ -124,146 +167,23 @@ export default function Panel(props) {
         </React.Fragment>
     )
 
-    return (
-        <Box className={classes.panel}>
-            <Box className={classes.infoArea} >
-                {(mode === 'learn') ? commentaryBoard : scoreBoard}
-            </Box>
-            <Box className={classes.controls} >
-                {(mode === 'learn') ? learnButtons : playButtons}
-            </Box>
-        </Box>
-    )
+    
 }
 
 
-function UndoButton(props) {
-    const classes = useStyles();
-    const gameOver = props.gameOver
-    const moveNumber = props.moveNumber
-    const handleUndoClick = props.handleUndoClick
-    return (
-        <Button
-            className={classes.button}
-            variant="contained"
-            color="primary"
-            onClick={() => handleUndoClick()}
-        // disabled={gameOver || moveNumber < 1}
-        >
-            <UndoIcon className={classes.buttonIcon} />
-            Undo
-        </Button>
-    )
-}
-
-function ShowHintsButton(props) {
-    const classes = useStyles();
-    const toggleShowHints = props.toggleShowHints
-
-    return (
-        <Button
-            className={classes.button}
-            variant="contained"
-            color="primary"
-            onClick={() => toggleShowHints()}
-        >
-            <HelpOutlineIcon className={classes.buttonIcon} />
-            Show Hints
-        </Button>
-    )
-}
 
 
-function NewGameButton(props) {
-    const classes = useStyles();
-    const handleNewGameClick = props.handleNewGameClick;
 
-    return (
-        <Button
-            className={classes.button}
-            variant="contained"
-            color="primary"
-            onClick={() => handleNewGameClick()}
-        // disabled={!props.gameOver}
-        >
-            <ReplayIcon className={classes.buttonIcon} />
-            New&nbsp;Game
-        </Button>
-    )
-}
-
-function HelpButton(props) {
-    const classes = useStyles();
-    const handleUndoButtonClick = props.handleUndoButtonClick
+    
 
 
-    return (
-        <Box className={classes.button} >
-            <HelpModal />
-        </Box>
-    )
-}
-
-function SettingsButton(props) {
-    const classes = useStyles();
-    const handleUndoButtonClick = props.handleUndoButtonClick
 
 
-    return (
-        <Box className={classes.button} >
-            <TicTacToeSettingsModal
-                showMoves={props.showMoves}
-                showCommentary={props.showCommentary}
-                toggleShowMovesSwitch={props.toggleShowMovesSwitch}
-                toggleShowCommentarySwitch={props.toggleShowCommentarySwitch}
-            />
-        </Box>
-    )
-}
 
 
-    // const classes = useStyles();
-
-    // const mode = props.mode
-    // const gameType = props.gameType;
-
-    // const gameOver = props.gameOver;
-    // const moveNumber = props.moveNumber;
-    // const gameStatus = props.gameStatus;
-    // const gameNumber = props.gameNumber;
-    // const commentary = props.commentary;
-
-    // const handleNewGameClick = props.handleNewGameClick
-    // const handleUndoClick = props.handleUndoClick
-
-    // const showHints = props.showHints
-    // const toggleShowHints = props.toggleShowHints
 
 
-    // const showCommentary = props.showCommentary
-    // const toggleShowCommentarySwitch = props.toggleShowCommentarySwitch
-
-    // return (
-    //     <Box className={classes.panel}>
-    //         <Box className={classes.infoArea} >
-    //             {/* <Typography align='center' component='h1' variant='h4' noWrap gutterBottom>
-    //             {gameStatus}
-    //             </Typography> */}
-    //             <Typography align='justify' variant='body1' >
-    //                 {commentary}
-    //             </Typography>
-    //         </Box>
-    //         <Box className={classes.controls} >
-    //             <UndoButton
-    //                 handleUndoClick={handleUndoClick}
-    //             />
-    //             <ShowHintsButton
-    //                 toggleShowHints={toggleShowHints}
-    //             />
-    //         </Box>
-    //     </Box>
-    // )
 
 
-   
+
 
