2d7896c919d4bed126209cec5a8601fc94521ccb
nolastemgarden <nolastemgarden@gmail.com>
2021-06-08 18:33:31 -0500

    Tidy up unused style classes

--- src/components/Panels/Parts/StatusHeader.js
+++ src/components/Panels/Parts/StatusHeader.js
@@ -2,23 +2,10 @@ import React from 'react';
 
 import { status } from "../../../logic/GameLogic";
 
-// Custom Components
-
 // MUI Components
 import Typography from '@material-ui/core/Typography';
 
-// Custom Styling
-import { makeStyles } from '@material-ui/core/styles';
-// import classes from '*.module.css';
-// const useStyles = makeStyles((theme) => ({
-//     root: {
-//         // border: 'solid yellow 1px',
-//         color: theme.palette.common.white,
-//     }
-// }));
-
 export default function StatusHeader(props) {
-    // const classes = useStyles();
     const gameStatus = status(props.moveList);
 
     function statusMessage(status) {
@@ -38,7 +25,6 @@ export default function StatusHeader(props) {
         }
     }
 
-     
     return (
         <Typography color="textPrimary" align='center' component='h1' variant='h4' noWrap gutterBottom>
             {statusMessage(gameStatus)}
