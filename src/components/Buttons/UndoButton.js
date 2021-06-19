c4fa9407db9db62090ccf7c009ba39bd78d4ff54
nolastemgarden <nolastemgarden@gmail.com>
2021-06-19 18:09:26 -0400

    Remove superfluous style classes, restyle button

--- src/components/Buttons/UndoButton.js
+++ src/components/Buttons/UndoButton.js
@@ -14,20 +14,11 @@ import UndoIcon from '@material-ui/icons/Undo';
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
     button: {
-        color: theme.palette.common.white,
         backgroundColor: theme.palette.primary.main,
-        margin: '0.5rem 5%',
-        width: '90%',
-        height: '3.0rem',
-        fontSize: '1.2rem',
+        width: '100%',
+        height: '2.5rem',
+        fontSize: '1.1rem',
     },
-    buttonIcon: {
-        marginRight: '1vmin',
-        fontSize: 'larger'
-        // fontSize: 'min(max(0.7rem, 3vmin), 22px)',
-    },
-
-
 }));
 
 export default function UndoButton(props) {
