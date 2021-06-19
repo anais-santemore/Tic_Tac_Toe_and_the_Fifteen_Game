e513e5d2c3b6441d6662fae0850d2d0fe3b090e6
nolastemgarden <nolastemgarden@gmail.com>
2021-06-19 18:03:30 -0400

    Remove superfluous style classes

--- src/components/Buttons/BotGoFirstButton.js
+++ src/components/Buttons/BotGoFirstButton.js
@@ -11,27 +11,13 @@ import Button from '@material-ui/core/Button';
 import '../../styles/App.css';
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
-    buttonArea: {
-        // border: 'solid green 1px',
-        padding: '1.5rem 0rem',
-    },
     button: {
         color: theme.palette.common.white,
         backgroundColor: theme.palette.primary.main,
-        // margin: '0.35rem 0rem',
         width: '100%',
         height: '2.5rem',
         fontSize: '1.1rem',
     },
-    selectedButton: {
-        color: theme.palette.common.white,
-        backgroundColor: 'rgba(46, 107, 18, 1.0)',
-    },
-    unselectedButton: {
-        color: '#999999',
-        backgroundColor: 'rgba(46, 107, 18, 0.5)',
-    },
-
 }));
 
 export default function DifficultyModeButtons(props) {
