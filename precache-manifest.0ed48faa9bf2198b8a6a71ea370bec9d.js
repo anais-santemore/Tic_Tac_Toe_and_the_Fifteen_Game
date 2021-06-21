a3ec8e312511a7159f58a0abbe5d31df95a0f187
nolastemgarden <nolastemgarden@gmail.com>
2021-06-21 13:52:44 -0400

    Updates

---
+++ precache-manifest.0ed48faa9bf2198b8a6a71ea370bec9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06fa7eb58959ed07fe9a63f8a6c3eb0b",
+    "url": "/tic-tac-toe-solution/index.html"
+  },
+  {
+    "revision": "b029930533d1d878198e",
+    "url": "/tic-tac-toe-solution/static/css/main.c9cb539e.chunk.css"
+  },
+  {
+    "revision": "626773beceedccac7f28",
+    "url": "/tic-tac-toe-solution/static/js/2.d38c4cbd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tic-tac-toe-solution/static/js/2.d38c4cbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b029930533d1d878198e",
+    "url": "/tic-tac-toe-solution/static/js/main.9424ecaf.chunk.js"
+  },
+  {
+    "revision": "3d46e30ff95b3813572e",
+    "url": "/tic-tac-toe-solution/static/js/runtime-main.c684bf45.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/tic-tac-toe-solution/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "a2170e487f6fb06a90b778b217b56a4f",
+    "url": "/tic-tac-toe-solution/static/media/nsgLogoSnipped.a2170e48.png"
+  }
+]);
