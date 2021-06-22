51904d9221232698000d0dddb05127cdb1c8f615
nolastemgarden <nolastemgarden@gmail.com>
2021-06-22 09:04:50 -0400

    Create SpacerBox.js
    Extracted from Navbar.js

---
+++ src/components/Navbar/SpacerBox.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+// MATERIAL-UI COMPONENTS
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const heightInRem = 3.6;
+const height = `${heightInRem}rem`;
+const halfHeight = `${heightInRem / 2}rem`;
+
+const useStyles = makeStyles((theme) => ({
+    spacerBox: {
+        border: 'solid blue 1px',
+        // paddingTop: halfHeight,
+        paddingTop: height,
+        paddingLeft: '99vw',
+        // paddingX: '50vw',
+        position: 'absolute',
+        top: 0,
+    },
+}));
+
+
+export default function SpacerBox(props) {
+    const classes = useStyles();
+    return (
+        <Box className={classes.spacerBox} />
+    )
+}
+
