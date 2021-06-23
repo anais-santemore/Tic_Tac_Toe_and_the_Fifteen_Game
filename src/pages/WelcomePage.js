704df418830c0c95f1fc24063baa100bc72f4cac
nolastemgarden <nolastemgarden@gmail.com>
2021-06-23 09:29:23 -0400

    Update WelcomePage.js

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -5,21 +5,17 @@ import {
 } from "react-router-dom";
 
 // My Components
-import SpacerBox from "../components/Navbar/SpacerBox";
 
 // MUI  components
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography'
-
 
 // Custom Styling
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
-    
     root: {
         // borderRight: 'solid black 1rem',
         width: '100%',
@@ -29,49 +25,51 @@ const useStyles = makeStyles((theme) => ({
         flexDirection: 'column',
         color: theme.palette.common.white,
         padding: '1.0rem',
-        
     }, 
-    
     button: {
-        // margin: 'auto',
         backgroundColor: theme.palette.primary.main,
         textAlign: 'center',
         width: '95%',
-
-        // margin: '1.0rem',
     }, 
-    
-    
 }));
-
 
 export default function WelcomePage() {
     const classes = useStyles();
-    
     
     return (
         <Container className={classes.root} maxWidth='md' >
             <Box py={3} />
             <Typography align='center' color='textPrimary' component='h1' variant='h2' gutterBottom >
-                Tic-Tac-Toe & Proof
+                Tic-Tac-Toe & The Fifteen Game
             </Typography>
             <Grid container>
                 <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
-                    Proof: An Expression of Creativity
+                    Tic-Tac-Toe as an Intro to Proof
                 </Typography>
                 <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
-                    Logical proof is the foundation that all Math sits upon. 
-                    Math is a human endeavor, an expression of creativity.
-                    Just like a musician expresses their creativity in a song, a mathematician expresses their creativity in a 
-                    logical argument. As with songs, there are proofs that are considered classic, there are those that are seen as 
-                    modern innovations on the classics, and others that are competely novel.
-                    Mathematical proofs can be profoundly beautiful and even elegant, however, to the untrained eye, this 
-                    beauty and elegance can be difficult to see because proofs often involve a lot of abstraction and formal 
-                    definitions. 
+                    Proof is the foundation that all Mathematics sits upon, but the practice of writing "proofs" is it is often
+                    Mathematics is not just a static set of rules to memorize, it is a subject that is still growing with new discoveries.
+                    It is a human endeavor, an expression of creativity. 
+                    Mathematical proofs can be beautiful and elegant, however, to the untrained eye, this can be difficult to pick up on
+                    because proofs often involve a lot of abstraction and formal definitions. 
+                </Typography>
+                <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+                    The current usual way students are introduced to the practice of writing "proofs" is
+                    in the context of a geometry class, while simultaneously being introduced to similar and congruent
+                    triangles. Proof should be introduced in a context that is simple and familiar.
+                    My favorite way is to use the classic game Tic Tac Toe. I challenge kids to write down a detailed stategy that the
+                    first player can use to win everytime. If they have played enough Tic Tac Toe to have
+                    realized that the game usually ends in a draw, and they say "There is no such winning strategy!" then I ask them
+                    to proove that instead. (It can be done with pencil and paper, it will take several pages and will challenge
+                    your ability to organize your thoughts. It's much more than just playing lots of games and recording the results.
+                    This website includes a different way of expressing this proof in the Play with Coach section.)
+                    Once proof is understood in a familiar context, then kids can extend that understanding into whatever domain they
+                    desire. Understanding how to rigorously critique your own argument and data is key to being in conntrol of your own
+                    education.
                 </Typography>
                 
                 <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
-                    Tic-Tac-Toe as an Intro to Proof
+                    The Fifteen Game 
                 </Typography>
                 <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
                     I think it is a mistake to introduce students to the practice of writing "proofs" in the current usual way: 
@@ -161,3 +159,9 @@ export default function WelcomePage() {
         </Container>
     );
 }
+
+
+
+// Just like a musician expresses their creativity in a song, a mathematician expresses their creativity in a
+// logical argument.As with songs, there are proofs that are considered classic, there are those that are seen as
+//     modern innovations on the classics, and others that are competely novel.
