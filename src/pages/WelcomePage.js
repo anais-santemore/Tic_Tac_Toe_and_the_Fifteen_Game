d375218f4be6ee6687d3453b19d9ea9cd7750321
nolastemgarden <nolastemgarden@gmail.com>
2021-06-24 15:20:50 -0400

    Update WelcomePage.js

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -82,26 +82,8 @@ export default function WelcomePage() {
                     It is essential in the process of becoming an educated person to reflect on what it takes for you to be convinced. 
                     This exercise is a great way to wrap your mind around the difference between a rigorous proof and a reasonable explanation.
                 </Typography>
-                
-                <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
-                    The Fifteen Game
-                </Typography>
-                <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
-                    I think it is a mistake to introduce students to the practice of writing "proofs" in the current usual way: 
-                    in the context of a highschool geometry class, while simultaneously being introduced to similar and congruent 
-                    triangles. Proof should be introduced in a context that is simple and familiar. 
-                    My favorite way is to use the classic game Tic Tac Toe. I challenge kids to write down a detailed stategy that the 
-                    first player can use to win everytime. If they have played enough Tic Tac Toe to have 
-                    realized that the game usually ends in a draw, and they say "There is no such winning strategy!" then I ask them 
-                    to proove that instead. (It can be done with pencil and paper, it will take several pages and will challenge 
-                    your ability to organize your thoughts. It's much more than just playing lots of games and recording the results.
-                    This website includes a different way of expressing this proof in the Play with Coach section.)
-                    Once proof is understood in a familiar context, then kids can extend that understanding into whatever domain they 
-                    desire. Understanding how to rigorously critique your own argument and data is key to being in conntrol of your own 
-                    education.
-                </Typography>
                 <Box width="100%" mb={3} >
-                    <Grid container> 
+                    <Grid container>
                         <Grid item xs={12} sm={4} >
                             <Button
                                 className={classes.button}
@@ -138,27 +120,18 @@ export default function WelcomePage() {
                     </Grid>
                 </Box>
                 <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
-                    Defining Your Terms
+                    The Fifteen Game
                 </Typography>
                 <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
-                    In this lesson we will use certain words that we have and intuitive grasp on: mistake, threat, double-attack. We need to go beyond our intuitive 
-                    understanding of these words and define them in a precice way. I leave writing and refining these definitions as an exercise for the student. 
-                    Keep in mind, a good definition is an "if and only if" statement, it outlines the conditions which are both necessary and sufficient. 
+                 
+                 
+                 
+                    GET FROM OTHER REPO  !!!!!
+                
+                
                 </Typography>
-                <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
-                    Proof on Paper
-                </Typography>
-                <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
-                    It may take you several pages, but you can prove using an exhaustive tree-diagram that in classic Tic-Tac-Toe both players worst case scenario
-                    if they avoid making any mistakes is a draw. If you get stuck or want to check your answers, take a look at the "Learn Tic Tac Toe Strategy" page.
-                </Typography>
-                <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
-                    Taking It Further
-                </Typography>
-                <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
-                    Is it possible for the player who goes first to make a mistake on the first move of the game? 
-                    Is is possible for the player who goes second to make a mistake on their first move?
-                </Typography>
+                
+                
                 
                 
                 <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
@@ -180,3 +153,26 @@ export default function WelcomePage() {
 // Just like a musician expresses their creativity in a song, a mathematician expresses their creativity in a
 // logical argument.As with songs, there are proofs that are considered classic, there are those that are seen as
 //     modern innovations on the classics, and others that are competely novel.
+
+{/* <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
+    Defining Your Terms
+</Typography>
+<Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+    In this lesson we will use certain words that we have and intuitive grasp on: mistake, threat, double-attack. We need to go beyond our intuitive 
+    understanding of these words and define them in a precice way. I leave writing and refining these definitions as an exercise for the student. 
+    Keep in mind, a good definition is an "if and only if" statement, it outlines the conditions which are both necessary and sufficient. 
+</Typography>
+<Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
+    Proof on Paper
+</Typography>
+<Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+    It may take you several pages, but you can prove using an exhaustive tree-diagram that in classic Tic-Tac-Toe both players worst case scenario
+    if they avoid making any mistakes is a draw. If you get stuck or want to check your answers, take a look at the "Learn Tic Tac Toe Strategy" page.
+</Typography>
+<Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
+    Taking It Further
+</Typography>
+<Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+    Is it possible for the player who goes first to make a mistake on the first move of the game? 
+    Is is possible for the player who goes second to make a mistake on their first move?
+</Typography> */}
