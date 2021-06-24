c52492bc491f5925fb729112d7b4d196b854ca5a
nolastemgarden <nolastemgarden@gmail.com>
2021-06-24 15:16:23 -0400

    Update WelcomePage.js

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -44,32 +44,47 @@ export default function WelcomePage() {
             </Typography>
             <Grid container>
                 <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
+                    A Math Lesson in Disguise
+                </Typography>
+                <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+                    Mathematics is not just a bunch of rules to memorize, it is a human endeavor, an expression of our creativity, 
+                    a shared inheritance that has been added to by every culture around the world and throughout time. 
+                    Humans have been doing math for a long time (about 20 thousand years) and yet the subject is still growing,
+                    with new theorems being proven every year. Mathematical proofs can be beautiful and elegant, however, 
+                    this can be difficult to pick up on for someone with an untrained eye because proofs are often written in a way 
+                    that involves a lot of abstraction and formality.
+                </Typography>
+                <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
                     Tic-Tac-Toe as an Intro to Proof
                 </Typography>
                 <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
-                    Proof is the foundation that all Mathematics sits upon, but the practice of writing "proofs" is it is often
-                    Mathematics is not just a static set of rules to memorize, it is a subject that is still growing with new discoveries.
-                    It is a human endeavor, an expression of creativity. 
-                    Mathematical proofs can be beautiful and elegant, however, to the untrained eye, this can be difficult to pick up on
-                    because proofs often involve a lot of abstraction and formal definitions. 
+                    Proof is the foundation that all Mathematics sits upon, but the practice of writing "proofs" is often
+                    taught quite poorly. This results in people never really coming to understand how a proof is different
+                    than meerly "showing your work". In American schools, students are typically asked to write their first
+                    proofs in the context of a geometry class, at the same time as they are being introduced to a lot of new
+                    concepts and vocabulary dealing with similar and congruent triangles.
                 </Typography>
                 <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
-                    The current usual way students are introduced to the practice of writing "proofs" is
-                    in the context of a geometry class, while simultaneously being introduced to similar and congruent
-                    triangles. Proof should be introduced in a context that is simple and familiar.
-                    My favorite way is to use the classic game Tic Tac Toe. I challenge kids to write down a detailed stategy that the
-                    first player can use to win everytime. If they have played enough Tic Tac Toe to have
-                    realized that the game usually ends in a draw, and they say "There is no such winning strategy!" then I ask them
-                    to proove that instead. (It can be done with pencil and paper, it will take several pages and will challenge
-                    your ability to organize your thoughts. It's much more than just playing lots of games and recording the results.
-                    This website includes a different way of expressing this proof in the Play with Coach section.)
-                    Once proof is understood in a familiar context, then kids can extend that understanding into whatever domain they
-                    desire. Understanding how to rigorously critique your own argument and data is key to being in conntrol of your own
-                    education.
+                    This is a mistake, in my opinion. Proof is such an important foundation that it should be introduced in a
+                    context that is simple and familiar. My favorite way to introduce the concept of proof makes use of the
+                    classic game Tic Tac Toe. I challenge students to write down a detailed stategy that the first player can
+                    use to win everytime. If they have played enough Tic Tac Toe to have realized that the game usually ends in
+                    a draw, and they say "There is no such winning strategy!" then I ask them
+                    to proove that instead.
+                </Typography>
+                <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+                    This can be done with nothing more than pencil and paper! I've done it and I encourage
+                    you to give it a try. It will take several pages and it will challenge your ability to organize your thoughts.
+                    It's going to take more than just playing lots of games and recording the results. Try making a tree-diagram
+                    that maps out all variations. There are shortcuts you can take that will depend on symmetry. You may find it helpful
+                    to develop your own shorthand notation for a position. You will certainly need to come up with a precice definition
+                    of the word "mistake". 
+                    It is essential in the process of becoming an educated person to reflect on what it takes for you to be convinced. 
+                    This exercise is a great way to wrap your mind around the difference between a rigorous proof and a reasonable explanation.
                 </Typography>
                 
                 <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
-                    The Fifteen Game 
+                    The Fifteen Game
                 </Typography>
                 <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
                     I think it is a mistake to introduce students to the practice of writing "proofs" in the current usual way: 
