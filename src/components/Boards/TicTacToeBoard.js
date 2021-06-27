75bdc74fe2b422ee2bdc083cea63e22a31f69ee7
nolastemgarden <nolastemgarden@gmail.com>
2021-06-27 13:13:45 -0400

    Update TicTacToeBoard.js

--- src/components/Boards/TicTacToeBoard.js
+++ src/components/Boards/TicTacToeBoard.js
@@ -11,6 +11,11 @@ import {
     availableNumbers,
     moveListStringToArray
 } from "../../logic/GameLogic";
+import {
+    highlightWins,
+    getBoardIcons,
+    getBoardHints,
+} from "../../logic/BoardLogic";
 
 // My Components
 import Square from "./Square";
@@ -22,22 +27,22 @@ import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
     board: {
-        // border: 'solid green 1px',
-        width: 'max(50vh, 100%)',
-        height: '50vh',
+        width: 'min(50vh, 100%)',
+        height: '100%',
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'space-between',
+        justifyContent: 'space-around',
         alignItems: 'center',
     },
     row: {
         // border: 'solid purple 1px',
-        width: '95%',  
-        height: '31%',
+        width: '100%',  
+        height: '15vh',
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        justifyContent: 'space-around',
     },
+    
 }));
 
 export default function Board(props) {
@@ -46,60 +51,6 @@ export default function Board(props) {
     let moveList = props.moveList
     let gameStatus = status(moveList)
 
-    function getBoardIcons(mls) {
-        let data = Array(10).fill('_');  // Start with an array representing a board of NINE empty squares
-        let mla = moveListStringToArray(mls)
-        mla.forEach((squareId, turn) => {
-            data[squareId] = (turn % 2 === 0) ? 'x' : 'o'
-        })
-        return data;  // this method only deals with current board position, not hypotheticals.  Thus, it wants to use a version of helper squaresClaimedByPlayer() that does not require a moveList be explicitly passed in. 
-    }
-    function getBoardColors(mls) {
-        if (gameStatus === "xWins" || gameStatus === "oWins") {
-            return highlightWins(mls)
-        }
-        else if (props.showHints === true) {
-            return getBoardHints(mls)
-        }
-        else {
-            return Array(10).fill('noColor')
-        }
-    }
-
-    function highlightWins(ml) {
-        let colors = Array(10).fill('noColor')
-        let Xs = xNumbers(ml)
-        let Os = oNumbers(ml)
-        let winningTrios = trioList.filter(trio =>
-            intersect(trio, Xs).length === 3 || intersect(trio, Os).length === 3
-        )
-        winningTrios.flat().forEach(num => colors[num] = 'win')
-        return colors
-    }
-    function getBoardHints(mls) {
-        let colors = Array(10).fill('noColor')
-        availableNumbers(mls).forEach(num => {
-            let outcome = outcomeMap.get(mls + num.toString())
-            colors[num] = getHintColor(outcome)
-        })
-        console.log(`COLORS: ${colors}`)
-        return colors
-    }
-
-    function getHintColor(outcome) {
-        if (outcome === "draw") {
-            return "draw"
-        }
-        else if (gameStatus === "xNext") {
-            return (outcome === "xWins") ? "win" : "lose"
-        }
-        else if (gameStatus === "oNext") {
-            return (outcome === "oWins") ? "win" : "lose"
-        }
-        else {
-            console.error(`Error in Get Hint Color`);
-        }
-    }
 
     let boardIcons = getBoardIcons(moveList)
     let boardColors = getBoardColors(moveList)
@@ -130,10 +81,21 @@ export default function Board(props) {
             </Box>
         </Box>
 
-        // <Grid container className={classes.board}>
-        //     {squares}
-        // </Grid>
+        
     )
+
+    function getBoardColors(ml) {
+        let gameStatus = status(ml)
+        if (gameStatus === "xWins" || gameStatus === "oWins") {
+            return highlightWins(ml)
+        }
+        else if (props.showHints === true) {
+            return getBoardHints(ml)
+        }
+        else {
+            return Array(10).fill('noColor')
+        }
+    }
 }
 
 
