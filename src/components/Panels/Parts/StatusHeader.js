dbe6684e31835f7c51ec0c1002655e23a730ad71
nolastemgarden <nolastemgarden@gmail.com>
2021-06-29 15:36:19 -0400

    Add different message for 15-G mode

--- src/components/Panels/Parts/StatusHeader.js
+++ src/components/Panels/Parts/StatusHeader.js
@@ -9,19 +9,37 @@ export default function StatusHeader(props) {
     const gameStatus = status(props.moveList);
 
     function statusMessage(status) {
-        switch (status) {
-            case "xWins":
-                return "Game Over. X Wins!"
-            case "oWins":
-                return "Game Over. O Wins!"
-            case "draw":
-                return "Game Over! Draw."
-            case "xNext":
-                return "It is X's turn."
-            case "oNext":
-                return "It is O's turn."
-            default:
-                return "Error";
+        if (props.game === 'tic-tac-toe') {
+            switch (status) {
+                case "xWins":
+                    return "Game Over. X Wins!"
+                case "oWins":
+                    return "Game Over. O Wins!"
+                case "draw":
+                    return "Game Over! Draw."
+                case "xNext":
+                    return "It is X's turn."
+                case "oNext":
+                    return "It is O's turn."
+                default:
+                    return "Error";
+            }
+        }
+        else if (props.game === 'fifteen-game') {
+            switch (status) {
+                case "xWins":
+                    return "Game Over. Red Wins!"
+                case "oWins":
+                    return "Game Over. Blue Wins!"
+                case "draw":
+                    return "Game Over! Draw."
+                case "xNext":
+                    return "It is Red's turn."
+                case "oNext":
+                    return "It is Blue's turn."
+                default:
+                    return "Error";
+            }
         }
     }
 
